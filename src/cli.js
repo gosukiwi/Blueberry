2cb30f33ed55dec5515889138998f5bc42a84995
Federico Ramirez <fedekiller@gmail.com>
2015-06-01 02:55:16 -0300

    Bump version

--- src/cli.js
+++ src/cli.js
@@ -21,7 +21,7 @@
         compileFile,
         walk,
         commands,
-        VERSION = '0.6.1';
+        VERSION = '0.6.2';
 
     /*
      * Gets the command line argument with the given index
